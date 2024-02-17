09e50368eb374ca7cd25ed654ed342d5a4259139
duhso <duhso@JosPlcido>
2024-02-17 10:44:15 -0300

    projetos iniciais

---
+++ Java/src/ProjetosIniciais/ContaBasica3.java
@@ -0,0 +1,17 @@
+package ProjetosIniciais;
+
+public class ContaBasica3 {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		double resultado;
+
+		a = 226;
+		b = 53;
+		resultado = (double) a / b;
+
+		System.out.printf("resultado: %.2f%n", resultado);
+	}
+
+}
---
+++ Java/src/ProjetosIniciais/ContaBásica2.java
@@ -0,0 +1,18 @@
+package ProjetosIniciais;
+
+public class ContaBásica2 {
+
+	public static void main(String[] args) {
+
+		int x;
+		double y;
+
+		x = 5;
+		y = 2 * x;
+
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
